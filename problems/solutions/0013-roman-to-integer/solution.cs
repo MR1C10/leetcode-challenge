8b67ed3f212a514b322b9967775d9c3ac8b40387
MR1C10 <mauriciorodriguesoficial@gmail.com>
2025-07-02 00:48:49 -0300

    Implement RomanToInt solution in C#

--- problems/solutions/0013-roman-to-integer/solution.cs
+++ problems/solutions/0013-roman-to-integer/solution.cs
@@ -1,5 +1,36 @@
-// public class Solution {
-//     public int RomanToInt(string s) {
-//         
-//     }
-// }
+public class Solution {
+    public int RomanToInt(string s) {
+
+        Dictionary<char, int> romanos = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int resultadoFinal = 0;
+        int valorAntes = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int valorAtual = romanos[s[i]]; // pegando o valor da  letra atual
+            
+            // se o valor atual for menor que o anterior subtrai
+            if (valorAtual < valorAntes)
+            {
+                resultadoFinal -= valorAtual; // ex: IV(4), CD(400)
+            }
+            else // caso contrario soma
+            {
+                resultadoFinal += valorAtual;
+            }
+
+            valorAntes = valorAtual;
+        }
+        return resultadoFinal;
+    }
+}
